b5bb743d9f76947a0843b8ebb18408a056f8ef4e
Daniel Yates <me@meorawr.io>
2024-04-20 16:48:07 +0100

    Initial Cataclysm support (but without data)

---
+++ Data_Cata.lua
@@ -0,0 +1,15 @@
+local _, _addon = ...
+local buildNr = 0
+
+-- \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+local AtlasInfo = {}
+
+-- /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+-- Replace content in this area
+-- Make sure the 'return AtlasInfo' at the end is not included
+--------------------------------------------
+
+-- Don't remove this!
+_addon.data = AtlasInfo
+_addon.dataBuild = buildNr
